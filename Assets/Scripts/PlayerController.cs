bfa37aef7b851d9060c0914d737493f683c5b670
John1018 <sihua.john@gmail.com>
2024-12-08 13:09:38 +0800

    Implement animation
    Need to add transform

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
 
     // 地面檢測
     public Transform groundCheck; // 檢查地面的位置
+    public Transform spriteTransform;
     public float groundCheckRadius = 1.5f; // 檢查範圍
     public float groundCheckDistance = 1.5f;
     public LayerMask groundLayer; // 定義地面圖層
@@ -20,6 +21,7 @@ public class PlayerController : MonoBehaviour
     const float Tolerance = 0.05f;//離地跳躍
     float m_CoyoteTime; // coyote time 的剩餘時間
     Animator animator;
+    private int animationState = 0;
     //animator.SetInteger("State", 1);
     private void Start()
     {
@@ -36,6 +38,11 @@ public class PlayerController : MonoBehaviour
         // 左右移動
         float moveInput = Input.GetAxis("Horizontal"); // 獲取鍵盤輸入（-1到1）
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y); // 設置速度
+        if (moveInput > 0) {
+            spriteTransform.localScale = new Vector3(1, 1, 1); // 恢復正常方向
+        } else if (moveInput < 0) {
+            spriteTransform.localScale = new Vector3(-1, 1, 1); // 水平翻轉
+        }
         // 檢測是否在地面
         bool isGrounded = CheckGround();
         //Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
@@ -44,6 +51,23 @@ public class PlayerController : MonoBehaviour
         } else {
             m_CoyoteTime -= Time.deltaTime; // 如果沒有站在地面上就開始倒數
         }
+        //移動動畫
+        if (isGrounded) {
+            if (moveInput > 0) {
+                animationState = 2;
+            } else if (moveInput < 0) {
+                animationState = 3;
+            } else {
+                animationState = rb.velocity.x >= 0 ? 0 : 1;
+            }
+        } else {
+            if (moveInput > 0) {
+                animationState = 4;
+            } else if (moveInput < 0) {
+                animationState = 5;
+            }
+        }
+
         // 跳躍
         if (Input.GetKeyDown(KeyCode.Space) && m_CoyoteTime > 0)
         {
@@ -51,23 +75,56 @@ public class PlayerController : MonoBehaviour
         }
         //如果按下z/x，觸發對應加或乘，並會在按下方向鍵後，確定移動方向
         //但就算長按也只會觸發一次
-        //GameManager.instance.plus();
         if (press_multi){
+            
             if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)){
+                if(GameManager.instance.levelManager.plat_dict.ContainsKey(counter))
+                {
+                    animationState = rb.velocity.x >= 0 ? 6 : 7;
+                }
+                else
+                {
+                    animationState = rb.velocity.x >= 0 ? 10 : 11;
+                }
                 GameManager.instance.multi(counter++, true);
                 press_multi = false;
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+                if(GameManager.instance.levelManager.plat_dict.ContainsKey(counter))
+                {
+                    animationState = rb.velocity.x >= 0 ? 6 : 7;
+                }
+                else
+                {
+                    animationState = rb.velocity.x >= 0 ? 10 : 11;
+                }
                 GameManager.instance.multi(counter++, false);
                 press_multi = false;
             }
         }
         else if(press_plus){
+            
             if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)){
+                if(GameManager.instance.levelManager.plat_dict.ContainsKey(counter))
+                {
+                    animationState = rb.velocity.x >= 0 ? 8 : 9;
+                }
+                else
+                {
+                    animationState = rb.velocity.x >= 0 ? 10 : 11;
+                }
                 GameManager.instance.plus(counter++, true);
                 press_plus = false;
             }
             else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)){
+                if(GameManager.instance.levelManager.plat_dict.ContainsKey(counter))
+                {
+                    animationState = rb.velocity.x >= 0 ? 8 : 9;
+                }
+                else
+                {
+                    animationState = rb.velocity.x >= 0 ? 10 : 11;
+                }
                 GameManager.instance.plus(counter++, false);
                 press_plus = false;
             }
@@ -82,7 +139,7 @@ public class PlayerController : MonoBehaviour
                 press_multi = true;
             }
         }
-
+        animator.SetInteger("State", animationState);
     }
     private void FixedUpdate()
     {
